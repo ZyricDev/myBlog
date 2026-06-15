61c670e958f56505380adc4ee42a86d310f53677
Amirhossein Zareei <amirhossein2004zareei>
2026-06-15 14:53:12 +0330

    feat: add validation middleware for request schema validation.

---
+++ src/shared/middleware/validate.js
@@ -0,0 +1,33 @@
+import AppError from "../errors/AppError.js";
+
+const validate = (schema) => {
+  return (req, res, next) => {
+    const partsToValidate = ["body", "params", "query"];
+    const errors = [];
+
+    partsToValidate.forEach((part) => {
+      if (schema[part]) {
+        const { error } = schema[part].validate(req[part], {
+          abortEarly: false,
+        });
+
+        if (error) {
+          errors.push(...error.details);
+        }
+      }
+    });
+
+    if (errors.length > 0) {
+      let formattedError = errors.map((detail) => ({
+        field: detail.path[0],
+        message: detail.message.replace(/"/g, ""),
+      }));
+
+      throw new AppError("Validation failed", 400, formattedError);
+    }
+
+    next();
+  };
+};
+
+export default validate;
