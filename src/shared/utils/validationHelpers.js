4bd27532319c78f949ffac900da7def8d70cd6d8
Amirhossein Zareei <amirhossein2004zareei>
2026-06-17 17:05:05 +0330

    feat: add createBodyObjectSchema for request body validation.

---
+++ src/shared/utils/validationHelpers.js
@@ -0,0 +1,10 @@
+import joi from "joi";
+
+const createBodyObjectSchema = (fields) => {
+  return joi.object(fields).required().unknown(false).messages({
+    "object.base": "Request body must be an object",
+    "any.required": "Request body is required",
+  });
+};
+
+export  { createBodyObjectSchema };
