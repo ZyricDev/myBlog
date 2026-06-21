abf7b8bb3697b05d4901c9e3abadca8a3d899e9d
Amirhossein Zareei <amirhossein2004zareei>
2026-06-21 09:52:21 +0330

    feat: add admin email configuration and validation for required environment variables.

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,8 @@
 const config = {
+  admin: {
+    email: process.env.SUPER_ADMIN_EMAIL,
+  },
+
   app: {
     port: process.env.PORT || 4000,
     nodeEnv: process.env.NODE_ENV || "development",
@@ -7,9 +11,28 @@ const config = {
   auth: {
     accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY,
     refreshTokenSecretKey: process.env.REFRESH_TOKEN_SECRET_KEY,
-    accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
-    refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || 15, //m
+    refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || 14, //d
   },
 };
+
+const requiredConfigVars = [
+  { name: "SUPER_ADMIN_EMAIL", value: config.admin.email },
+  { name: "ACCESS_TOKEN_SECRET_KEY", value: config.auth.accessTokenSecretKey },
+  {
+    name: "REFRESH_TOKEN_SECRET_KEY",
+    value: config.auth.refreshTokenSecretKey,
+  },
+];
+
+const missingVars = requiredConfigVars.filter((env) => !env.value);
+
+if (missingVars.length > 0) {
+  const missingNames = missingVars.map((env) => env.name).join("\n");
+
+  throw new Error(
+    `🔥 Server startup stopped!\n The following critical variables are not set in the .env file:\n${missingNames}`,
+  );
+}
 
 export default config;
